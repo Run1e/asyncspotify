90090751291b3826328548faa186b1a33e4a27ac
Anna <anna11199810@hotmail.co.uk>
2020-03-24 23:02:51 +0000

    Make AudioAnalysis seconds fields timedelta object

--- asyncspotify/audioanalysis.py
+++ asyncspotify/audioanalysis.py
@@ -1,14 +1,22 @@
-from datetime import timedelta
+from .utils	import convertToTimeDelta
 from .object import Object
 
 class AudioAnalysis(Object):
+	'''
+	Class for Audio Analysis results from the Spotify API.
+    
+	'''
 	_type = 'audio_analysis'
 
 	def __init__(self, client, data):
 		super().__init__(client, data)
+		# List of seconds fields in data
+		listOfFieldsToConvert = ['start','duration','offset_seconds','window_seconds','start_of_fade_out','loudness_max_time']
+		data = convertToTimeDelta(data, listOfFieldsToConvert)
 		self.bars = data.pop('bars')
 		self.track = data.pop('track')
 		self.beats = data.pop('beats')
 		self.tatums = data.pop('tatums')
 		self.sections = data.pop('sections')
 		self.segments = data.pop('segments')
+
